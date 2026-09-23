85e8c51961bec7701262be6a93cb5c878b9d6d5a
tr3n1ttty <tr3n1ttty@192.168.1.5>
2026-09-23 14:53:57 +0200

    training utils: sgd exps

--- cs336_basics/training_utils.py
+++ cs336_basics/training_utils.py
@@ -1,4 +1,7 @@
 import torch
+from torch.optim import Optimizer
+from typing import Callable
+import math
 
 
 def cross_entropy(logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
@@ -15,3 +18,55 @@ def cross_entropy(logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
     under_mean = next_token_logits - log_sum_exps
     result = - torch.mean(under_mean)
     return result
+
+
+class SGD(Optimizer):
+    def __init__(self, params, lr: float = 1e-3):
+        if lr < 0:
+            raise ValueError(f"Invalid lr argument: {lr}")
+
+        defaults = {"lr": lr}
+        super().__init__(params, defaults)
+
+    def step(self, clousure: Callable | None = None):
+        loss = None if clousure is None else clousure()
+        for group in self.param_groups:
+            lr = group["lr"]
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+
+                state = self.state[p]
+                t = state.get("t", 0)
+                grad = p.grad.data
+                p.data -= lr / math.sqrt(t + 1) * grad
+                state["t"] = t + 1
+
+        return loss
+
+torch.manual_seed(666)
+weights = torch.nn.Parameter(5 * torch.randn((10, 10)))
+# opt = SGD([weights], lr=1)
+
+# for t in range(100):
+#     opt.zero_grad()
+#     loss = (weights ** 2).mean()
+#     print(loss.cpu().item())
+#     loss.backward()
+#     opt.step()
+
+def try_different_lrs(lr, n=10):
+    opt = SGD([weights], lr=1)
+
+    for t in range(n):
+        opt.zero_grad()
+        loss = (weights ** 2).mean()
+        print(loss.cpu().item())
+        loss.backward()
+        opt.step()
+
+try_different_lrs(lr=1e1)  # >> 17.85977554321289 -> 14.779157638549805, decrease only
+print('~' * 66)
+try_different_lrs(lr=1e2)  # >> 14.592806816101074 -> 12.075705528259277, decrease only
+print('~' * 66)
+try_different_lrs(lr=1e3)  # >> 11.923442840576172 -> 9.866779327392578, decrease only
